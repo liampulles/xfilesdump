6bb359ccb79e120a9a9d8a3dba8f33646c8e6f6c
sl1msn1per <sl1msn1per@live.com>
2017-02-17 11:40:03 +0200

    slightly impproves barnes.py

--- barnes.py
+++ barnes.py
@@ -12,3 +12,19 @@ page = urllib.request.urlopen(url).read().decode("utf-8")
 rawchaps = page.split(r'<h2>Scene Index</h2>')[1].split('<article>')[1].split(r'</article>')[0][1:]
 chapslist = rawchaps.split('<br>')
 print(chapslist)
+
+discs = []
+disc_count = 0
+ep_count = 0
+for i in chapslist:
+    if i[0] != ' ':
+        disc_count += 1
+        discs.add([])
+    elif i[0:1] == '1.':
+        ep_count += 1
+        discs[disc_count].add([])
+        discs[disc_count][ep_count].add(i)
+    else:
+        discs[disc_count][ep_count].add(i)
+
+print(discs)
