bb8ab92ba7c3a7d1431f08c11ce43a0b1a04ece1
sl1msn1per <sl1msn1per@live.com>
2017-02-17 16:44:33 +0000

    making progress

--- barnes.py
+++ barnes.py
@@ -1,30 +1,49 @@
-#!/usr/bin/python
-
+#Banres and Noble functions module
 import urllib.request
 
-#Ask for url
-url = input("Barnes & Noble URL? ")
-page = urllib.request.urlopen(url).read().decode("utf-8")
+def barnesget(url):
+    page = urllib.request.urlopen(url).read().decode("utf-8")
 
-#start = page.find(r'<h2>Scene Index</h2>')
-#line = page.find(r'<article>',start)
-#endline = page.find(r'</article>',line)
-rawchaps = page.split(r'<h2>Scene Index</h2>')[1].split('<article>')[1].split(r'</article>')[0][1:]
-chapslist = rawchaps.split('<br>')
-print(chapslist)
+    #start = page.find(r'<h2>Scene Index</h2>')
+    #line = page.find(r'<article>',start)
+    #endline = page.find(r'</article>',line)
+    rawchaps = page.split(r'<h2>Scene Index</h2>')[1].split('<article>')[1].split(r'</article>')[0][1:]
+    chapslist = rawchaps.split('<br>')
+    chapslist.pop()
+    for i in chapslist:
+        if i[0] == ' ':
+            chapslist[chapslist.index(i)] = i[1:]
+            i = i[1:]
+        if i.find('[') != -1:
+            chapslist[chapslist.index(i)] = i[0:i.find('[')-1]
 
-discs = []
-disc_count = 0
-ep_count = 0
-for i in chapslist:
-    if i[0] != ' ':
-        disc_count += 1
-        discs.add([])
-    elif i[0:1] == '1.':
-        ep_count += 1
-        discs[disc_count].add([])
-        discs[disc_count][ep_count].add(i)
-    else:
-        discs[disc_count][ep_count].add(i)
+    rawnames = page.split(r'<h2>Menu</h2>')[1].split('<article>')[1].split(r'</article>')[0][1:]
+    nameslist = rawnames.split('<br>')
+    nameslist2 = []
+    for i in nameslist:
+        if i.count(r'&nbsp; &nbsp;') == 1:
+            nameslist2.append(i.split(r'&nbsp; &nbsp;')[1])
+    nameslist = nameslist2
+    #print(nameslist)
 
-print(discs)
+    discs = []
+    episodes = []
+    disc_count = 0
+    ep_count = 0
+    for i in chapslist:
+        if i[0:4] == 'Disc':
+            discs.append({'episodes':[]})
+            disc_count = len(discs) - 1
+        elif i[0:2] == '1.':
+            discs[disc_count]['episodes'].append({})
+            episodes.append(disc_count)
+            ep_count = len(discs[disc_count]['episodes']) - 1
+            discs[disc_count]['episodes'][ep_count]['number'] = len(episodes);
+            discs[disc_count]['episodes'][ep_count]['title'] = nameslist[len(episodes)-1]
+            discs[disc_count]['episodes'][ep_count]['chapters'] = []
+            discs[disc_count]['episodes'][ep_count]['chapters'].append(i)
+        else:
+            discs[disc_count]['episodes'][ep_count]['chapters'].append(i)
+
+    #print(discs)
+    return discs, episodes
--- ripper.py
+++ ripper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-
 from subprocess import Popen, PIPE
-import ast
+import ast, barnes
+
+#Get Barnes and noble info
+#discs, episodes = barnes.barnesget(input("Barnes & Noble URL? "))
 
 #Get info from disc as string
 process = Popen(["lsdvd","/dev/sr0","-x","-Oy"], stdout=PIPE)
@@ -9,5 +11,36 @@ process = Popen(["lsdvd","/dev/sr0","-x","-Oy"], stdout=PIPE)
 exit_code = process.wait()
 
 #Parse dictionary from string
-dvdinfo = ast.literal_eval(output[8:])
-print(dvdinfo['longest_track'])
+dvdinfo = ast.literal_eval(output[8:].decode("utf-8"))
+
+#Disc Info
+discno = dvdinfo['title'].split('DISC')[1][0]
+season = dvdinfo['title'].split('_S')[1][0]
+
+titlecat(dvdinfo['title'][0])
+
+def titlecat(titleset):
+    #Is it an episode?
+    if len(titleset['chapters']) > 6:
+        return 'episode'
+
+    #If not, have user profile:
+    #1) International clip (episode)
+    #2) Deleted Scene (episode)
+    #3) Episode extra
+    #4) Season extra
+    #5) Irrelevant (don't keep)
+    else:
+        #Play in mpv
+        print("Playing with MPV...")
+        Popen(["mpv",'dvd://'+str(titleset['ix'])])
+        print("Stopped Playing with MPV.")
+        option = input("""
+        Was that:
+            1) An International clip
+            2) A Deleted Scene
+            3) An Episode extra
+            4) A Season extra
+            5) Irrelevant (don't keep)
+        :""")
+        print(option)
